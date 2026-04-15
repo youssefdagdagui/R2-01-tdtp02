c14cc7d7972f1f393ab9e5be68d4758d0e30f435
youssefdagdagui <youssefdagdagui0@gmail.com>
2026-04-15 21:03:21 +0100

    Add files via upload

---
+++ Exercice1.java
@@ -0,0 +1,7 @@
+public class Exercice1 {
+    public static void main(String[] args) {
+        String phrase = new String("Ceci est un exemple");
+        String resultat = phrase.replace('e', 'u');
+        System.out.println(resultat);
+    }
+}
