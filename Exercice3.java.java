c68f903663f783ee04da85f277344fedc25ef48b
youssefdagdagui <youssefdagdagui0@gmail.com>
2026-04-19 22:58:46 +0100

    Add files via upload

---
+++ Exercice3.java.java
@@ -0,0 +1,11 @@
+class Exercice3 {
+
+    public static void main(String[] args) {
+
+        String phrase = new String("Ceci est une phrase.");
+
+        boolean resultat = phrase.endsWith(".");
+
+        System.out.println(resultat);
+    }
+}
