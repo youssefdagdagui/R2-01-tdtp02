de5a056e7de36dcf18734138a21ea12c04205e56
youssefdagdagui <youssefdagdagui0@gmail.com>
2026-04-19 23:09:28 +0100

    Add files via upload

---
+++ Exercice5.java
@@ -0,0 +1,10 @@
+class Exercice5 {
+    public static void main(String[] args) {
+        String phrase = new String("ceci est une chaine de caractere");
+
+        // toUpperCase transforms the whole string to uppercase
+        String result = phrase.toUpperCase();
+
+        System.out.println(result);
+    }
+}
